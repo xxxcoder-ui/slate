d52a92c02784c8d1ca56de0574fc3d2069f36b14
toast <toastts@icloud.com>
2021-03-26 15:09:57 -0700

    increased storage cap for users

--- node_common/constants.js
+++ node_common/constants.js
@@ -6,10 +6,10 @@ export const FILE_STORAGE_URL = "./public/static/files/";
 export const GITHUB_URL = "https://github.com/filecoin-project/slate";
 export const ANALYTICS_URL = "https://slate-stats-dev.azurewebsites.net/";
 
-// NOTE(jim): 4 GB from Ignacio
-export const TEXTILE_ACCOUNT_BYTE_LIMIT = 1073741824 * 4;
+// NOTE(toast): 30 GB from jim/martina/ignacio
+export const TEXTILE_ACCOUNT_BYTE_LIMIT = 1073741824 * 30;
 
-// NOTE(jim): 4 GB - minus .textileseed
+// NOTE(jim): 30 GB - minus .textileseed
 export const TEXTILE_BUCKET_LIMIT = TEXTILE_ACCOUNT_BYTE_LIMIT - 234;
 
 // NOTE(jim): 100mb
