2686a082261c050fae20f8322b0d46649b968e26
Aminejvm <elouartinra@hotmail.com>
2021-03-26 19:28:04 +0100

    added RotateCcw icon

--- common/svg.js
+++ common/svg.js
@@ -1821,3 +1821,23 @@ export const AlignRight = (props) => (
     />
   </svg>
 );
+
+export const RotateCcw = (props) => (
+  <svg
+    width={24}
+    height={24}
+    viewBox="0 0 24 24"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+    {...props}
+  >
+    <path d="M1 4v6h6" stroke="#000" strokeWidth={2} strokeLinecap="round" strokeLinejoin="round" />
+    <path
+      d="M3.51 15a9 9 0 102.13-9.36L1 10"
+      stroke="#000"
+      strokeWidth={2}
+      strokeLinecap="round"
+      strokeLinejoin="round"
+    />
+  </svg>
+);
