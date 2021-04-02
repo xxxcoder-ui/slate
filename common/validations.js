d1d43f3569e6bde2196b68014413cdc924acf754
Akuoko Daniel Jnr <danielakuokojnr@gmail.com>
2021-04-02 15:00:27 +0000

    fix: detect modern unity webgl version

--- common/validations.js
+++ common/validations.js
@@ -157,9 +157,11 @@ export const isUnityFile = async (file) => {
     const fileNames = Object.keys(contents.files);
 
     // NOTE(daniel): every Unity game file will have this file
-    const unityRegex = new RegExp(/unityloader.js/i);
+    // NOTE(daniel): every Unity game file will have this file
+    const isUnityLoaderFile = (fileName) =>
+      [/unityloader.js/i, /(.*)\.loader.js/i].some((item) => item.test(fileName));
 
-    return fileNames.some((file) => unityRegex.test(file));
+    return fileNames.some((file) => isUnityLoaderFile(file));
   } catch (e) {
     return false;
   }
