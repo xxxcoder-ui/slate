782439fcc8fa93d219fe0ca92c3b3a140bed4ab6
Aminejvm <elouartinra@hotmail.com>
2021-05-16 19:46:48 +0100

    updated: /verifications/create to use pin

--- pages/api/verifications/create.js
+++ pages/api/verifications/create.js
@@ -1,7 +1,8 @@
 import * as Data from "~/node_common/data";
 import * as Validations from "~/common/validations";
-
-import crypto from "crypto";
+import * as Strings from "~/common/strings";
+import * as Environment from "~/node_common/environment";
+import * as Utilities from "~/common/utilities";
 
 export default async (req, res) => {
   if (!Strings.isEmpty(Environment.ALLOWED_HOST) && req.headers.host !== Environment.ALLOWED_HOST) {
@@ -10,14 +11,18 @@ export default async (req, res) => {
       .send({ decorator: "SERVER_CREATE_VERIFICATION_NOT_ALLOWED", error: true });
   }
 
-  if (!Validations.username(req.body.data.username)) {
+  if (!Validations.email(req.body?.data?.email)) {
     return res
       .status(500)
       .send({ decorator: "SERVER_CREATE_VERIFICATION_INVALID_EMAIL", error: true });
   }
 
-  const code = crypto.randomBytes(5).toString("hex");
-  const verification = await Data.createVerification({ email: req.body.email, code: code });
+  const userEmail = req.body.data.email.toLowerCase();
+
+  const pin = Utilities.generateRandomNumberInRange(111111, 999999);
+  const verification = await Data.createVerification({ email: userEmail, pin });
+
+  // TODO: send email to the users
 
   if (!verification) {
     return res.status(404).send({ decorator: "SERVER_CREATE_VERIFICATION_FAILED", error: true });
@@ -27,6 +32,6 @@ export default async (req, res) => {
   }
   return res.status(200).send({
     decorator: "SERVER_CREATE_VERIFICATION",
-    user: { email: verification.email, sid: verification.sid },
+    token: verification.sid,
   });
 };
