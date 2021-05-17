a550c92f3f08f3e3462574cd00d021dcb95dc4a3
toast <toastts@icloud.com>
2021-05-17 10:04:45 -0700

    moved email functions to email manager

---
+++ node_common/managers/emails.js
@@ -0,0 +1,62 @@
+import * as Environment from "~/node_common/environment";
+const sgMail = require("@sendgrid/mail");
+sgMail.setApiKey(Environment.SENDGRID_API_KEY);
+
+import "isomorphic-fetch";
+
+//NOTE(toast): please see https://sendgrid.com/docs/api-reference/
+//for sendgrid request structure, see what's optional
+//also see https://github.com/sendgrid/sendgrid-nodejs/tree/main/packages/mail
+
+export const sendEmail = async ({personalizations, to, from, subject, content, optionalData = {}}) => {
+  const msg = {
+    personalizations:personalizations,
+    to: to,
+    from: from,
+    subject: subject,
+    content: content,
+    optionalData: optionalData,
+  };
+  try {
+    await sgMail.send(msg);
+  } catch (error) {
+    res.status(500).send({ decorator: "SEND_EMAIL_FAILURE", error: true });
+  }
+};
+
+//NOTE(toast): templates override content, subject, etc.
+//properties that are defined in the template take priority
+export const sendTemplate = async ({to, from, templateId, templateData}) => {
+  const msg = {
+    to: to,
+    from: from,
+    templateId: templateId,
+    dynamic_template_data: { ...templateData },
+  };
+  try {
+    await sgMail.send(msg);
+  } catch (error) {
+    res.status(500).send({ decorator: "SEND_TEMPLATE_EMAIL_FAILURE", error: true });
+  }
+};
+
+//NOTE(toast): only available to upgraded sendgrid accounts
+//uses their validation service to make sure an email is legit
+export const validateEmail = async (req, res) => {
+  const msg = { email: req.body.email };
+  const request = await fetch("https://api.sendgrid.com/v3/validations/email", {
+    method: "POST",
+    headers: {
+      Authorization: Environment.SENDGRID_API_KEY,
+      Accept: "application/json",
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(msg),
+  });
+
+  try {
+    await request();
+  } catch (e) {
+    res.status(500).send({ decorator: "VALIDATE_EMAIL_FAILURE", error: true });
+  }
+};
