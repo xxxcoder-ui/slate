df44ec3ea77a73d9e7db73c7d784f5d6bc2605e2
Aminejvm <elouartinra@hotmail.com>
2021-06-15 17:01:13 +0100

    feat(object-preview): align font preview to center

--- components/core/ActivityObjectPreview/ActivityFontPreview.js
+++ components/core/ActivityObjectPreview/ActivityFontPreview.js
@@ -3,51 +3,32 @@ import "isomorphic-fetch";
 import * as React from "react";
 import * as Styles from "~/common/styles";
 
-import { AspectRatio, SVG, P } from "~/components/system";
-import { useInView } from "~/common/hooks";
 import { css } from "@emotion/react";
 
 import ObjectPreviewPremitive from "./ObjectPreviewPremitive";
-import TextPlaceholder from "./placeholders/Text";
-import FontObjectPreview from "~/components/core/FontFrame/Views/FontObjectPreview";
 import { useFont } from "~/components/core/FontFrame/hooks";
 
-const STYLES_TAG = (theme) => css`
-  position: absolute;
-  text-transform: uppercase;
-  background-color: ${theme.system.bgLight};
-  bottom: 26%;
-  left: 50%;
-  transform: translateX(-50%);
-  padding: 2px 8px;
-  border-radius: 4px;
+const STYLES_TEXT_PREVIEW = (theme) => css`
+  position: relative;
+  height: 100%;
+  margin: 8px;
+  background-color: ${theme.system.white};
+  border-radius: 8px;
+  box-shadow: ${theme.shadow.large};
 `;
 
-const STYLES_TEXT_PREVIEW = (theme) =>
-  css({
-    display: "flex",
-    alignItems: "center",
-    justifyContent: "center",
-    position: "relative",
-    height: "100%",
-    margin: "8px",
-    top: "8px",
-    backgroundColor: "#FFF",
-    borderRadius: "8px",
-    boxShadow: theme.shadow.large,
-  });
-
 const STYLES_LETTER = (theme) => css`
+  transform: translateY(-25%);
   overflow: hidden;
   font-size: ${theme.typescale.lvl8};
 `;
 
-export default function ActivityFontPreview({ url, file, type, ...props }) {
-  const { isFontLoading, error, fontName } = useFont({ cid: file.cid }, [file.cid]);
+export default function ActivityFontPreview({ file, type, ...props }) {
+  const { fontName } = useFont({ cid: file.cid }, [file.cid]);
 
   return (
     <ObjectPreviewPremitive type={type} {...props}>
-      <div css={STYLES_TEXT_PREVIEW}>
+      <div css={[Styles.CONTAINER_CENTERED, STYLES_TEXT_PREVIEW]}>
         <div style={{ fontFamily: fontName }}>
           <div css={STYLES_LETTER}>Aa</div>
         </div>
