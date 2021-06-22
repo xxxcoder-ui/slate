ed2623249e8bf8741837b0d78f0de935268f2f39
Aminejv <elouartinra@hotmail.com>
2021-06-22 19:31:59 +0100

    feat(account-linking): add TwitterLinking component

--- components/core/Auth/components/AuthCheckBox.js
+++ components/core/Auth/components/AuthCheckBox.js
@@ -1,5 +1,6 @@
 import * as React from "react";
 import * as System from "~/components/system";
+
 import { css } from "@emotion/react";
 
 const STYLES_CHECKBOX_LABEL = (theme) => css`
@@ -28,20 +29,12 @@ const STYLES_CHECKBOX_ERROR = (theme) => css`
   width: 16px;
 `;
 
-const STYLES_CHECKBOX_SUCCESS = (theme) => css`
-  background-color: rgba(242, 242, 247, 0.5);
-  border: 1px solid ${theme.system.green};
-  height: 16px;
-  width: 16px;
-`;
-
 const STYLES_CHECKBOX_WRAPPER = css`
   align-items: center;
 `;
 
 export default function AuthCheckBox({ touched, error, ...props }) {
   const showError = touched && error;
-  const showSuccess = touched && !error;
 
   const STYLES_CHECKBOX = React.useMemo(() => {
     if (showError) return STYLES_CHECKBOX_ERROR;
--- components/core/Auth/index.js
+++ components/core/Auth/index.js
@@ -2,4 +2,5 @@ export { default as Signin } from "~/components/core/Auth/Signin";
 export { default as Signup } from "~/components/core/Auth/Signup";
 export { default as Initial } from "~/components/core/Auth/Initial";
 export { default as TwitterSignup } from "~/components/core/Auth/TwitterSignup";
+export { default as TwitterLinking } from "~/components/core/Auth/TwitterLinking";
 export { default as ResetPassword } from "~/components/core/Auth/ResetPassword";
