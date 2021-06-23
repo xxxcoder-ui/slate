7d45f048a3d5c78e7e6ae5d721886eaa395d7224
Aminejv <elouartinra@hotmail.com>
2021-06-23 11:03:52 +0100

    fix(field): add white border

--- components/core/Field.js
+++ components/core/Field.js
@@ -44,6 +44,7 @@ const STYLES_INPUT = (theme) => css`
   background-color: rgba(242, 242, 247, 0.7);
   box-shadow: ${theme.shadow.large};
   border-radius: 8px;
+  box-shadow: 0 0 0 1px ${theme.system.white};
   &::placeholder {
     color: ${theme.system.textGrayDark};
   }
