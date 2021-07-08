eda0e8a23499c4483b5701ccb5f10002af53ec34
Aminejv <elouartinra@hotmail.com>
2021-07-08 16:47:49 +0100

    fix(likeButton): use rgba value to get transparent effect

--- components/core/ObjectPreview/components/LikeButton.jsx
+++ components/core/ObjectPreview/components/LikeButton.jsx
@@ -7,28 +7,38 @@ import { css } from "@emotion/react";
 import { motion, useAnimation } from "framer-motion";
 
 const STYLES_BUTTON_HOVER = (theme) => css`
+  display: flex;
   :hover path {
     stroke: ${theme.system.brand};
   }
 `;
 
 const animate = async (controls) => {
-  await controls.start({ scale: 1.3, rotateY: 180, fill: Constants.system.brand });
+  await controls.start({ scale: 1.3, rotateY: 180, fill: "rgba(0, 132, 255, 1)" });
   await controls.start({ scale: 1 });
   controls.set({ rotateY: 0 });
 };
 
-export default function LikeButton(props) {
-  const [toggle, setToggle] = React.useState(false);
+const useMounted = (callback, depedencies) => {
+  const mountedRef = React.useRef(false);
+  React.useLayoutEffect(() => {
+    if (mountedRef.current && callback) {
+      callback();
+    }
+    mountedRef.current = true;
+  }, depedencies);
+};
+
+export default function LikeButton({ onClick, isLiked, ...props }) {
   const controls = useAnimation();
 
-  React.useLayoutEffect(() => {
-    if (toggle) {
+  useMounted(() => {
+    if (isLiked) {
       animate(controls);
       return;
     }
     controls.start({ fill: "#fff", scale: 1 });
-  }, [toggle]);
+  }, [isLiked]);
 
   return (
     <button
@@ -36,14 +46,14 @@ export default function LikeButton(props) {
       onClick={(e) => {
         e.preventDefault();
         e.stopPropagation();
-        setToggle((prev) => !prev);
+        if (onClick) onClick();
       }}
     >
       <motion.svg
         width={20}
-        height={21}
-        initial={{ fill: "white" }}
-        animate={{ fill: toggle ? Constants.system.black : Constants.system.white, ...controls }}
+        height={20}
+        initial={{ fill: isLiked ? "rgba(0, 132, 255, 1)" : "rgba(0, 132, 255, 0)" }}
+        animate={controls}
         transition={{ duration: 0.3 }}
         fill="none"
         xmlns="http://www.w3.org/2000/svg"
@@ -51,7 +61,7 @@ export default function LikeButton(props) {
       >
         <motion.path
           d="M17.367 4.342a4.584 4.584 0 00-6.484 0L10 5.225l-.883-.883a4.584 4.584 0 00-6.484 6.483l.884.883L10 18.192l6.483-6.484.884-.883a4.584 4.584 0 000-6.483v0z"
-          animate={{ stroke: toggle ? Constants.system.brand : Constants.system.textGrayDark }}
+          animate={{ stroke: isLiked ? Constants.system.brand : Constants.system.textGrayDark }}
           whileHover={{ stroke: Constants.system.brand }}
           strokeWidth={1.25}
           strokeLinecap="round"
