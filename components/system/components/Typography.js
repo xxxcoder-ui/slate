863f8b2f362bad46b165e80e0a842941a03b359b
Aminejv <elouartinra@hotmail.com>
2021-07-08 16:57:44 +0100

    feat(typography): add responsive styles, nbrOfLines and color props

--- components/system/components/Typography.js
+++ components/system/components/Typography.js
@@ -1,7 +1,7 @@
 import * as React from "react";
 import * as Constants from "~/common/constants";
-import * as Actions from "~/common/actions";
 import * as Strings from "~/common/strings";
+import * as Styles from "~/common/styles";
 
 import { css } from "@emotion/react";
 
@@ -17,6 +17,31 @@ const LINK_STYLES = `
     color: ${Constants.system.slate};
   }
 `;
+
+const useColorProp = (color) =>
+  React.useMemo(
+    () => (theme) => {
+      if (!color) return;
+      if (!(color in theme.system)) {
+        console.warn(`${color} doesn't exist in our design system`);
+        return;
+      }
+      return css({ color: theme.system[color] });
+    },
+    [color]
+  );
+
+const truncateElements = (nbrOfLines) =>
+  nbrOfLines &&
+  css`
+    overflow: hidden;
+    line-height: 1.5;
+    word-break: break-all;
+    text-overflow: ellipsis;
+    -webkit-line-clamp: ${nbrOfLines};
+    display: -webkit-box;
+    -webkit-box-orient: vertical;
+  `;
 
 const STYLES_LINK = css`
   ${LINK_STYLES}
@@ -58,7 +83,6 @@ export const Link = ({ href, children, dark }) => {
     target: isExternal(href) ? "_blank" : "_self",
     rel: isExternal(href) ? "external nofollow" : "",
     css: dark ? STYLES_LINK_DARK : STYLES_LINK,
-    children,
   };
 
   // process all types of Slate links
@@ -83,19 +107,19 @@ export const Link = ({ href, children, dark }) => {
     default: {
     }
   }
-  return <a {...linkProps} />;
+  return <a {...linkProps}>{children}</a>;
 };
 
 const STYLES_H1 = css`
   box-sizing: border-box;
-  font-size: ${Constants.typescale.lvl4};
-  line-height: 1.1;
-  font-family: ${Constants.font.semiBold};
-  font-weight: 400;
   color: inherit;
   text-decoration: none;
   display: block;
   overflow-wrap: break-word;
+  ${Styles.HEADING_01};
+  @media (max-width: ${Constants.sizes.mobile}px) {
+    ${Styles.HEADING_03}
+  }
 
   :hover {
     color: inherit;
@@ -113,20 +137,27 @@ const STYLES_H1 = css`
   ${ANCHOR}
 `;
 
-export const H1 = (props) => {
-  return <h1 css={STYLES_H1} {...props} />;
+export const H1 = ({ nbrOflines, children, color, ...props }) => {
+  const TRUNCATE_STYLE = React.useMemo(() => truncateElements(nbrOflines), [nbrOflines]);
+  const COLOR_STYLES = useColorProp(color);
+  return (
+    <h1 css={[STYLES_H1, TRUNCATE_STYLE, COLOR_STYLES]} {...props}>
+      {children}
+    </h1>
+  );
 };
 
 const STYLES_H2 = css`
   box-sizing: border-box;
-  font-size: ${Constants.typescale.lvl3};
-  line-height: 1.1;
-  font-family: ${Constants.font.medium};
-  font-weight: 400;
   overflow-wrap: break-word;
   color: inherit;
   text-decoration: none;
   display: block;
+
+  ${Styles.HEADING_02};
+  @media (max-width: ${Constants.sizes.mobile}px) {
+    ${Styles.HEADING_04}
+  }
 
   :hover {
     color: inherit;
@@ -144,20 +175,27 @@ const STYLES_H2 = css`
   ${ANCHOR}
 `;
 
-export const H2 = (props) => {
-  return <h2 css={STYLES_H2} {...props} />;
+export const H2 = ({ nbrOflines, children, color, ...props }) => {
+  const TRUNCATE_STYLE = React.useMemo(() => truncateElements(nbrOflines), [nbrOflines]);
+  const COLOR_STYLES = useColorProp(color);
+  return (
+    <h2 css={[STYLES_H2, TRUNCATE_STYLE, COLOR_STYLES]} {...props}>
+      {children}
+    </h2>
+  );
 };
 
 const STYLES_H3 = css`
   box-sizing: border-box;
-  font-size: ${Constants.typescale.lvl2};
-  line-height: 1.1;
-  font-family: ${Constants.font.medium};
-  font-weight: 400;
   overflow-wrap: break-word;
   color: inherit;
   text-decoration: none;
   display: block;
+
+  ${Styles.HEADING_03};
+  @media (max-width: ${Constants.sizes.mobile}px) {
+    ${Styles.HEADING_04}
+  }
 
   :hover {
     color: inherit;
@@ -175,20 +213,27 @@ const STYLES_H3 = css`
   ${ANCHOR}
 `;
 
-export const H3 = (props) => {
-  return <h3 css={STYLES_H3} {...props} />;
+export const H3 = ({ nbrOflines, children, color, ...props }) => {
+  const TRUNCATE_STYLE = React.useMemo(() => truncateElements(nbrOflines), [nbrOflines]);
+  const COLOR_STYLES = useColorProp(color);
+  return (
+    <h3 css={[STYLES_H3, TRUNCATE_STYLE, COLOR_STYLES]} {...props}>
+      {children}
+    </h3>
+  );
 };
 
 const STYLES_H4 = css`
   box-sizing: border-box;
-  font-size: ${Constants.typescale.lvl1};
-  line-height: 1.1;
-  font-family: ${Constants.font.medium};
-  font-weight: 400;
   overflow-wrap: break-word;
   color: inherit;
   text-decoration: none;
   display: block;
+
+  ${Styles.HEADING_04};
+  @media (max-width: ${Constants.sizes.mobile}px) {
+    ${Styles.HEADING_05}
+  }
 
   :hover {
     color: inherit;
@@ -206,15 +251,56 @@ const STYLES_H4 = css`
   ${ANCHOR}
 `;
 
-export const H4 = (props) => {
-  return <h4 css={STYLES_H4} {...props} />;
+export const H4 = ({ nbrOflines, children, color, ...props }) => {
+  const TRUNCATE_STYLE = React.useMemo(() => truncateElements(nbrOflines), [nbrOflines]);
+  const COLOR_STYLES = useColorProp(color);
+  return (
+    <h4 css={[STYLES_H4, TRUNCATE_STYLE, COLOR_STYLES]} {...props}>
+      {children}
+    </h4>
+  );
+};
+
+const STYLES_H5 = css`
+  box-sizing: border-box;
+  overflow-wrap: break-word;
+  color: inherit;
+  text-decoration: none;
+  display: block;
+
+  ${Styles.HEADING_05};
+  @media (max-width: ${Constants.sizes.mobile}px) {
+    ${Styles.HEADING_05}
+  }
+
+  :hover {
+    color: inherit;
+  }
+
+  :visited {
+    color: inherit;
+  }
+
+  strong {
+    font-family: ${Constants.font.semiBold};
+    font-weight: 400;
+  }
+
+  ${ANCHOR}
+`;
+
+export const H5 = ({ nbrOflines, children, color, ...props }) => {
+  const TRUNCATE_STYLE = React.useMemo(() => truncateElements(nbrOflines), [nbrOflines]);
+  const COLOR_STYLES = useColorProp(color);
+  return (
+    <h5 css={[STYLES_H5, TRUNCATE_STYLE, COLOR_STYLES]} {...props}>
+      {children}
+    </h5>
+  );
 };
 
 const STYLES_P = css`
   box-sizing: border-box;
-  font-family: ${Constants.font.text};
-  font-size: ${Constants.typescale.lvl1};
-  line-height: 1.5;
   overflow-wrap: break-word;
 
   strong,
@@ -226,8 +312,22 @@ const STYLES_P = css`
   ${ANCHOR}
 `;
 
-export const P = (props) => {
-  return <div css={STYLES_P} {...props} />;
+const constructResponsiveStyle = ([desktop, mobile]) => css`
+  ${desktop};
+  @media (max-width: ${Constants.sizes.mobile}px) {
+    ${mobile}
+  }
+`;
+const variants = {
+  "para-01": constructResponsiveStyle([Styles.PARA_01, Styles.PARA_02]),
+  "para-02": constructResponsiveStyle([Styles.PARA_02, Styles.PARA_03]),
+  "para-03": Styles.PARA_03,
+};
+
+export const P = ({ variant = "para-01", nbrOflines, color, ...props }) => {
+  const TRUNCATE_STYLE = React.useMemo(() => truncateElements(nbrOflines), [nbrOflines]);
+  const COLOR_STYLES = useColorProp(color);
+  return <div css={[STYLES_P, variants[variant], TRUNCATE_STYLE, COLOR_STYLES]} {...props} />;
 };
 
 const STYLES_UL = css`
