8098e1bd5da8b3dd0559452f823c75c19adaacaf
Aminejv <elouartinra@hotmail.com>
2021-07-12 20:51:02 +0100

    feat(ActivityGrouping): add support for SAVE_COPY

--- components/core/ActivityGroup/index.js
+++ components/core/ActivityGroup/index.js
@@ -8,22 +8,27 @@ import {
   ActivityProfileGroup,
 } from "~/components/core/ActivityGroup/components";
 
-const STYLES_GROUP_GRID = css`
+const STYLES_GROUP_GRID = (theme) => css`
   display: grid;
   grid-template-columns: 260px 1fr;
   grid-row-gap: 32px;
-  border-bottom: 1px solid #e5e5ea;
+  border-bottom: 1px solid ${theme.system.bgGray};
   padding-bottom: 24px;
 `;
 
 export default function ActivityGroup({ onAction, viewer, external, group }) {
   const { type } = group;
-  if (type === "CREATE_FILE" || type === "CREATE_SLATE_OBJECT" || type === "LIKE_FILE") {
+  if (
+    type === "CREATE_FILE" ||
+    type === "CREATE_SLATE_OBJECT" ||
+    type === "LIKE_FILE" ||
+    type === "SAVE_COPY"
+  ) {
     return <ActivityFileGroup viewer={viewer} onAction={onAction} group={group} />;
   }
 
   if (type === "CREATE_SLATE" || type === "SUBSCRIBE_SLATE") {
-    return <ActivityCollectionGroup onAction={onAction} group={group} />;
+    return <ActivityCollectionGroup onAction={onAction} viewer={viewer} group={group} />;
   }
 
   if (type === "SUBSCRIBE_USER") {
@@ -31,7 +36,8 @@ export default function ActivityGroup({ onAction, viewer, external, group }) {
       <ActivityProfileGroup onAction={onAction} viewer={viewer} external={external} group={group} />
     );
   }
-  console.log(group);
+
+  // TODO(amine): grouping for making files/slate public
   return (
     <div css={STYLES_GROUP_GRID}>
       <div>{type}</div>
