c40fb9887faa52f59d88d4902d6499d416ef3300
Aminejv <elouartinra@hotmail.com>
2021-07-20 11:22:09 +0100

    feat(objectPreview): move placeholders to objectPreview index

--- components/core/ObjectPreview/index.js
+++ components/core/ObjectPreview/index.js
@@ -1,18 +1,27 @@
 import * as React from "react";
 import * as Validations from "~/common/validations";
 import * as Strings from "~/common/strings";
+import * as Styles from "~/common/styles";
+import * as Utilities from "~/common/utilities";
+
+import { css } from "@emotion/react";
 
 import ImageObjectPreview from "./ImageObjectPreview";
-import VideoObjectPreview from "./VideoObjectPreview";
 import TextObjectPreview from "./TextObjectPreview";
-import PdfObjectPreview from "./PdfObjectPreview";
-import EpubObjectPreview from "./EpubObjectPreview";
-import AudioObjectPreview from "./AudioObjectPreview";
-import KeynoteObjectPreview from "./KeynoteObjectPreview";
-import DefaultObjectPreview from "./DefaultObjectPreview";
-import Object3DPreview from "./3dObjectPreview";
-import CodeObjectPreview from "./CodeObjectPreview";
 import FontObjectPreview from "./FontObjectPreview";
+
+// Note(amine): placeholders
+import PdfPlaceholder from "~/components/core/ObjectPreview/placeholders/PDF";
+import VideoPlaceholder from "~/components/core/ObjectPreview/placeholders/Video";
+import AudioPlaceholder from "~/components/core/ObjectPreview/placeholders/Audio";
+import EbookPlaceholder from "~/components/core/ObjectPreview/placeholders/EPUB";
+import KeynotePlaceholder from "~/components/core/ObjectPreview/placeholders/Keynote";
+import CodePlaceholder from "~/components/core/ObjectPreview/placeholders/Code";
+import Object3DPlaceholder from "~/components/core/ObjectPreview/placeholders/3D";
+import FilePlaceholder from "~/components/core/ObjectPreview/placeholders/File";
+
+// NOTE(amine): previews
+import ObjectPreviewPrimitive from "~/components/core/ObjectPreview/ObjectPreviewPrimitive";
 
 const ObjectPreview = ({ file, ...props }) => {
   const { type } = file.data;
@@ -24,27 +33,54 @@ const ObjectPreview = ({ file, ...props }) => {
   }
 
   if (type.startsWith("video/")) {
-    return <VideoObjectPreview file={file} url={url} {...props} />;
+    const tag = type.split("/")[1];
+    return (
+      <PlaceholderWrapper tag={tag} file={file} {...props}>
+        <VideoPlaceholder />
+      </PlaceholderWrapper>
+    );
   }
 
   if (Validations.isPdfType(type)) {
-    return <PdfObjectPreview file={file} {...props} />;
+    return (
+      <PlaceholderWrapper tag="PDF" file={file} {...props}>
+        <PdfPlaceholder />
+      </PlaceholderWrapper>
+    );
   }
 
   if (type.startsWith("audio/")) {
-    return <AudioObjectPreview file={file} {...props} />;
+    const tag = Utilities.getFileExtension(file.filename) || "audio";
+    return (
+      <PlaceholderWrapper tag={tag} file={file} {...props}>
+        <AudioPlaceholder />
+      </PlaceholderWrapper>
+    );
   }
 
   if (type === "application/epub+zip") {
-    return <EpubObjectPreview file={file} {...props} />;
+    return (
+      <PlaceholderWrapper tag="epub" file={file} {...props}>
+        <EbookPlaceholder />
+      </PlaceholderWrapper>
+    );
   }
 
   if (file.filename.endsWith(".key")) {
-    return <KeynoteObjectPreview file={file} {...props} />;
+    return (
+      <PlaceholderWrapper tag="keynote" file={file} {...props}>
+        <KeynotePlaceholder />
+      </PlaceholderWrapper>
+    );
   }
 
   if (Validations.isCodeFile(file.filename)) {
-    return <CodeObjectPreview file={file} {...props} />;
+    const tag = Utilities.getFileExtension(file.filename) || "code";
+    return (
+      <PlaceholderWrapper tag={tag} file={file} {...props}>
+        <CodePlaceholder />
+      </PlaceholderWrapper>
+    );
   }
 
   if (Validations.isFontFile(file.filename)) {
@@ -56,10 +92,29 @@ const ObjectPreview = ({ file, ...props }) => {
   }
 
   if (Validations.is3dFile(file.filename)) {
-    return <Object3DPreview file={file} {...props} />;
+    return (
+      <PlaceholderWrapper tag="3d" file={file} {...props}>
+        <Object3DPlaceholder />
+      </PlaceholderWrapper>
+    );
   }
 
-  return <DefaultObjectPreview file={file} {...props} />;
+  return (
+    <PlaceholderWrapper tag="file" file={file} {...props}>
+      <FilePlaceholder />
+    </PlaceholderWrapper>
+  );
 };
 
 export default React.memo(ObjectPreview);
+
+const STYLES_CONTAINER = css`
+  height: 100%;
+`;
+const PlaceholderWrapper = ({ children, ...props }) => {
+  return (
+    <ObjectPreviewPrimitive {...props}>
+      <div css={[Styles.CONTAINER_CENTERED, STYLES_CONTAINER]}>{children}</div>
+    </ObjectPreviewPrimitive>
+  );
+};
