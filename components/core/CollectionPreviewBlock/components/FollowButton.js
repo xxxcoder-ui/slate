499c57d93c1d79e8eff30395701f0ceff86f14bf
Aminejv <elouartinra@hotmail.com>
2021-07-26 17:43:54 +0100

    feat(CollectionPreview): update collection preview to new design

--- components/core/CollectionPreviewBlock/components/FollowButton.js
+++ components/core/CollectionPreviewBlock/components/FollowButton.js
@@ -4,15 +4,31 @@ import * as Styles from "~/common/styles";
 
 import { css } from "@emotion/react";
 import { motion, useAnimation } from "framer-motion";
+import { P3 } from "~/components/system";
+import { useMounted } from "~/common/hooks";
 
-const STYLES_BUTTON_HOVER = (theme) => css`
-  :hover .button_path {
-    stroke: ${theme.system.blue};
+const STYLES_BUTTON = (theme) => css`
+  display: flex;
+  background-color: ${theme.semantic.bgBlurWhite};
+  padding: 8px;
+  border-radius: 8px;
+  box-shadow: 0 0 0 1px ${theme.system.grayLight5}, ${theme.shadow.lightLarge};
+  transition: box-shadow 0.3s;
+  :hover {
+    box-shadow: 0 0 0 1px ${theme.system.pink}, ${theme.shadow.lightLarge};
+  }
+
+  path {
+    transition: stroke 0.3s;
+  }
+
+  :hover path {
+    stroke: ${theme.system.pink};
   }
 `;
 
-const STYLES_INLINE = css`
-  display: flex;
+const STYLES_DISABLED = css`
+  cursor: not-allowed;
 `;
 
 const animate = async (controls) => {
@@ -20,17 +36,7 @@ const animate = async (controls) => {
   await controls.start({ x: 0, y: 0 });
 };
 
-const useMounted = (callback, depedencies) => {
-  const mountedRef = React.useRef(false);
-  React.useLayoutEffect(() => {
-    if (mountedRef.current && callback) {
-      callback();
-    }
-    mountedRef.current = true;
-  }, depedencies);
-};
-
-export default function FollowButton({ onFollow, isFollowed, disabled, ...props }) {
+export default function FollowButton({ onFollow, isFollowed, disabled, followCount, ...props }) {
   const controls = useAnimation();
 
   useMounted(() => {
@@ -41,45 +47,66 @@ export default function FollowButton({ onFollow, isFollowed, disabled, ...props 
   }, [isFollowed]);
 
   return (
-    <button
-      css={[Styles.BUTTON_RESET, STYLES_INLINE, !disabled && STYLES_BUTTON_HOVER]}
+    <motion.button
+      css={[Styles.BUTTON_RESET, STYLES_BUTTON, disabled && STYLES_DISABLED]}
+      initial={{
+        backgroundColor: isFollowed ? Constants.system.redLight6 : Constants.semantic.bgBlurWhite,
+      }}
+      animate={{
+        backgroundColor: isFollowed ? Constants.system.redLight6 : Constants.semantic.bgBlurWhite,
+      }}
       onClick={(e) => {
-        if (disabled) return;
+        if (disabled) {
+          return;
+        }
         e.preventDefault();
         e.stopPropagation();
         if (onFollow) onFollow();
       }}
     >
-      <svg
-        width={20}
-        height={20}
-        viewBox="0 0 20 20"
-        fill="none"
-        xmlns="http://www.w3.org/2000/svg"
-        {...props}
-      >
-        <motion.path
-          d="M3.33334 9.66669C5.32247 9.66669 7.23012 10.4569 8.63664 11.8634C10.0432 13.2699 10.8333 15.1776 10.8333 17.1667"
-          animate={{ stroke: isFollowed ? Constants.system.blue : "#000002" }}
-          strokeWidth={1.25}
-          strokeLinecap="round"
-          strokeLinejoin="round"
-        />
-        <motion.path
-          d="M3.33334 3.83331C6.86956 3.83331 10.2609 5.23807 12.7614 7.73856C15.2619 10.239 16.6667 13.6304 16.6667 17.1666"
-          animate={{ stroke: isFollowed ? Constants.system.blue : "#000002" }}
-          strokeWidth={1.25}
-          strokeLinecap="round"
-          strokeLinejoin="round"
-        />
-        <motion.path
-          d="M4.16668 17.1667C4.62691 17.1667 5.00001 16.7936 5.00001 16.3333C5.00001 15.8731 4.62691 15.5 4.16668 15.5C3.70644 15.5 3.33334 15.8731 3.33334 16.3333C3.33334 16.7936 3.70644 17.1667 4.16668 17.1667Z"
-          animate={{ stroke: isFollowed ? Constants.system.blue : "#000002" }}
-          strokeWidth={1.25}
-          strokeLinecap="round"
-          strokeLinejoin="round"
-        />
-      </svg>
-    </button>
+      <span css={Styles.HORIZONTAL_CONTAINER_CENTERED}>
+        <svg
+          width={20}
+          height={20}
+          viewBox="0 0 20 20"
+          fill="none"
+          xmlns="http://www.w3.org/2000/svg"
+          {...props}
+        >
+          <motion.path
+            d="M3.33334 9.66669C5.32247 9.66669 7.23012 10.4569 8.63664 11.8634C10.0432 13.2699 10.8333 15.1776 10.8333 17.1667"
+            initial={{ stroke: isFollowed ? Constants.system.pink : Constants.system.black }}
+            animate={{ stroke: isFollowed ? Constants.system.pink : Constants.system.black }}
+            strokeWidth={1.25}
+            strokeLinecap="round"
+            strokeLinejoin="round"
+          />
+          <motion.path
+            d="M3.33334 3.83331C6.86956 3.83331 10.2609 5.23807 12.7614 7.73856C15.2619 10.239 16.6667 13.6304 16.6667 17.1666"
+            initial={{ stroke: isFollowed ? Constants.system.pink : Constants.system.black }}
+            animate={{ stroke: isFollowed ? Constants.system.pink : Constants.system.black }}
+            strokeWidth={1.25}
+            strokeLinecap="round"
+            strokeLinejoin="round"
+          />
+          <motion.path
+            d="M4.16668 17.1667C4.62691 17.1667 5.00001 16.7936 5.00001 16.3333C5.00001 15.8731 4.62691 15.5 4.16668 15.5C3.70644 15.5 3.33334 15.8731 3.33334 16.3333C3.33334 16.7936 3.70644 17.1667 4.16668 17.1667Z"
+            initial={{ stroke: isFollowed ? Constants.system.pink : Constants.system.black }}
+            animate={{ stroke: isFollowed ? Constants.system.pink : Constants.system.black }}
+            strokeWidth={1.25}
+            strokeLinecap="round"
+            strokeLinejoin="round"
+          />
+        </svg>
+        <P3
+          as={motion.p}
+          style={{ marginLeft: 4, y: -0.5 }}
+          initial={{ color: isFollowed ? Constants.system.pink : Constants.semantic.textGrayDark }}
+          animate={{ color: isFollowed ? Constants.system.pink : Constants.semantic.textGrayDark }}
+        >
+          {followCount}
+        </P3>
+      </span>
+    </motion.button>
   );
 }
--- components/core/CollectionPreviewBlock/index.js
+++ components/core/CollectionPreviewBlock/index.js
@@ -1,31 +1,298 @@
 import * as React from "react";
 import * as Validations from "~/common/validations";
+import * as Typography from "~/components/system/components/Typography";
+import * as Styles from "~/common/styles";
+import * as Strings from "~/common/strings";
+import * as Constants from "~/common/constants";
+import * as SVG from "~/common/svg";
 
-import ImageCollectionPreview from "./ImageCollectionPreview";
-import FilesCollectionPreview from "./FilesCollectionPreview";
+import { Logo } from "~/common/logo";
+import { useInView } from "~/common/hooks";
+import { isBlurhashValid } from "blurhash";
+import { Blurhash } from "react-blurhash";
+import { css } from "@emotion/react";
+import { FollowButton } from "~/components/core/CollectionPreviewBlock/components";
+import { useFollowHandler } from "~/components/core/CollectionPreviewBlock/hooks";
+import { Link } from "~/components/core/Link";
+import { AnimatePresence, motion } from "framer-motion";
 
-export default function CollectionPreview({ collection, viewer, owner, onAction }) {
-  const objects = collection.objects.filter((file) =>
-    Validations.isPreviewableImage(file.data.type)
-  );
+import ObjectPlaceholder from "~/components/core/ObjectPreview/placeholders";
 
-  if (objects.length > 0) {
+const STYLES_CONTAINER = (theme) => css`
+  position: relative;
+  display: flex;
+  flex-direction: column;
+  background-color: ${theme.semantic.bgLight};
+  box-shadow: 0 0 0 0.5px ${theme.semantic.bgGrayLight}, ${theme.shadow.lightSmall};
+  border-radius: 16px;
+  width: 100%;
+  overflow: hidden;
+
+  height: 304px;
+  @media (max-width: ${theme.sizes.mobile}px) {
+    height: 281px;
+  }
+`;
+
+const STYLES_PREVIEW = css`
+  flex-grow: 1;
+  background-size: cover;
+  overflow: hidden;
+  img {
+    height: 100%;
+    width: 100%;
+    object-fit: cover;
+  }
+`;
+
+const STYLES_DESCRIPTION_CONTAINER = (theme) => css`
+  display: flex;
+  flex-direction: column;
+  position: relative;
+  padding: 9px 16px 12px;
+  border-radius: 0px 0px 16px 16px;
+  box-shadow: 0 -0.5px 0.5px ${theme.semantic.bgGrayLight};
+  width: 100%;
+  margin-top: auto;
+`;
+
+const STYLES_SPACE_BETWEEN = css`
+  justify-content: space-between;
+`;
+
+const STYLES_PROFILE_IMAGE = (theme) => css`
+  background-color: ${theme.semantic.bgLight};
+  height: 16px;
+  width: 16px;
+  border-radius: 4px;
+  object-fit: cover;
+`;
+
+const STYLES_METRICS = (theme) => css`
+  margin-top: 7px;
+  @media (max-width: ${theme.sizes.mobile}px) {
+    margin-top: 12px;
+  }
+  ${Styles.CONTAINER_CENTERED};
+  ${STYLES_SPACE_BETWEEN}
+`;
+
+const STYLES_PLACEHOLDER_CONTAINER = css`
+  height: 100%;
+  width: 100%;
+`;
+
+const STYLES_EMPTY_CONTAINER = css`
+  display: flex;
+  overflow: hidden;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  height: 100%;
+  width: 100%;
+`;
+
+const STYLES_CONTROLS = css`
+  position: absolute;
+  z-index: 1;
+  right: 16px;
+  top: 16px;
+  & > * + * {
+    margin-top: 8px !important;
+  }
+`;
+const STYLES_TEXT_GRAY = (theme) => css`
+  color: ${theme.semantic.textGray};
+`;
+
+const getFileBlurHash = (file) => {
+  const coverImage = file?.data?.coverImage;
+  const coverImageBlurHash = coverImage?.data?.blurhash;
+  if (coverImage && isBlurhashValid(coverImageBlurHash)) return coverImageBlurHash;
+
+  const blurhash = file?.data?.blurhash;
+  if (isBlurhashValid(blurhash)) return blurhash;
+
+  return null;
+};
+
+const getObjectToPreview = (objects = []) => {
+  let objectIdx = 0;
+  let isImage = false;
+
+  objects.some((object, i) => {
+    const isPreviewableImage = Validations.isPreviewableImage(object.data.type);
+    if (isPreviewableImage) (objectIdx = i), (isImage = true);
+    return isPreviewableImage;
+  });
+
+  return { ...objects[objectIdx], isImage };
+};
+
+const STYLES_DESCRIPTION_INNER = (theme) => css`
+  background-color: ${theme.semantic.bgLight};
+  border-radius: 16px;
+`;
+
+const Preview = ({ collection, children, ...props }) => {
+  const [isLoading, setLoading] = React.useState(true);
+  const handleOnLoaded = () => setLoading(false);
+
+  const previewerRef = React.useRef();
+  const { isInView } = useInView({
+    ref: previewerRef,
+  });
+
+  const isCollectionEmpty = collection.fileCount === 0;
+  if (isCollectionEmpty) {
     return (
-      <ImageCollectionPreview
-        collection={{ ...collection, objects }}
-        viewer={viewer}
-        owner={owner}
-        onAction={onAction}
-      />
+      <div css={STYLES_EMPTY_CONTAINER} {...props}>
+        {children}
+        <Logo style={{ height: 18, marginBottom: 8 }} />
+        <Typography.P1 color="textGrayDark">No files in this collection</Typography.P1>
+      </div>
+    );
+  }
+
+  const object = getObjectToPreview(collection.objects);
+  if (object.isImage) {
+    const { coverImage } = object.data;
+    const blurhash = getFileBlurHash(object);
+    const previewImage = coverImage
+      ? Strings.getURLfromCID(coverImage?.cid)
+      : Strings.getURLfromCID(object.cid);
+
+    return (
+      <div ref={previewerRef} css={STYLES_PREVIEW} {...props}>
+        {children}
+        {isInView && (
+          <>
+            {isLoading && blurhash && (
+              <Blurhash
+                hash={blurhash}
+                style={{ position: "absolute", top: 0, left: 0 }}
+                height="100%"
+                width="100%"
+                resolutionX={32}
+                resolutionY={32}
+                punch={1}
+              />
+            )}
+            <img src={previewImage} alt="Collection preview" onLoad={handleOnLoaded} />
+          </>
+        )}
+      </div>
     );
   }
 
   return (
-    <FilesCollectionPreview
-      collection={collection}
-      viewer={viewer}
-      owner={owner}
-      onAction={onAction}
-    />
+    <div css={STYLES_PREVIEW} {...props}>
+      {children}
+      <ObjectPlaceholder ratio={1} containerCss={STYLES_PLACEHOLDER_CONTAINER} file={object} />
+    </div>
+  );
+};
+
+export default function CollectionPreview({ collection, viewer, owner, onAction }) {
+  const [areControlsVisible, setShowControls] = React.useState(false);
+  const showControls = () => setShowControls(true);
+  const hideControls = () => setShowControls(false);
+
+  // const [isBodyVisible, setShowBody] = React.useState(false);
+  // const showBody = () => setShowBody(true);
+  // const hideBody = () => setShowBody(false);
+  // const body = collection?.data?.body;
+
+  const { follow, followCount, isFollowed } = useFollowHandler({ collection, viewer });
+
+  const { fileCount } = collection;
+
+  return (
+    <div css={STYLES_CONTAINER}>
+      <Preview collection={collection} onMouseEnter={showControls} onMouseLeave={hideControls}>
+        <AnimatePresence>
+          {areControlsVisible && (
+            <motion.div
+              initial={{ opacity: 0 }}
+              animate={{ opacity: 1 }}
+              exit={{ opacity: 0 }}
+              css={STYLES_CONTROLS}
+            >
+              <FollowButton
+                onClick={follow}
+                isFollowed={isFollowed}
+                followCount={followCount}
+                disabled={collection.ownerId === viewer?.id}
+              />
+            </motion.div>
+          )}
+        </AnimatePresence>
+      </Preview>
+      <div
+        css={STYLES_DESCRIPTION_CONTAINER}
+        //  onMouseEnter={showBody} onMouseLeave={hideBody}
+      >
+        <div
+          css={STYLES_DESCRIPTION_INNER}
+          // initial={{ y: 0 }}
+          // animate={{ y: isBodyVisible ? -170 : 0 }}
+          // transition={{ type: "spring", stiffness: 170, damping: 26 }}
+        >
+          <div css={[Styles.HORIZONTAL_CONTAINER_CENTERED, STYLES_SPACE_BETWEEN]}>
+            <Typography.H5 color="textBlack" nbrOflines={1}>
+              {collection.slatename}
+            </Typography.H5>
+          </div>
+
+          {/* {isBodyVisible && (
+            <div
+            style={{ marginTop: 4 }}
+            initial={{ opacity: 0 }}
+            animate={{ opacity: isBodyVisible ? 1 : 0 }}
+            >
+              <Typography.P2 color="textGrayDark" nbrOflines={5}>
+                {body || "sorry, no description available."}
+              </Typography.P2>
+            </div>
+          )} */}
+        </div>
+
+        <div css={STYLES_METRICS}>
+          <div css={[Styles.CONTAINER_CENTERED, STYLES_TEXT_GRAY]}>
+            <SVG.Box />
+            <Typography.P3 style={{ marginLeft: 4 }} color="textGray">
+              {fileCount}
+            </Typography.P3>
+          </div>
+          {owner && (
+            <div style={{ alignItems: "end" }} css={Styles.CONTAINER_CENTERED}>
+              <Link
+                href={`/$/user/${owner.id}`}
+                onAction={onAction}
+                aria-label={`Visit ${owner.username}'s profile`}
+                title={`Visit ${owner.username}'s profile`}
+              >
+                <img
+                  css={STYLES_PROFILE_IMAGE}
+                  src={owner?.data?.photo}
+                  alt={`${owner.username} profile`}
+                  onError={(e) => (e.target.src = Constants.profileDefaultPicture)}
+                />
+              </Link>
+              <Link
+                href={`/$/user/${owner.id}`}
+                onAction={onAction}
+                aria-label={`Visit ${owner.username}'s profile`}
+                title={`Visit ${owner.username}'s profile`}
+              >
+                <Typography.P3 style={{ marginLeft: 8 }} color="textGray">
+                  {owner.username}
+                </Typography.P3>
+              </Link>
+            </div>
+          )}
+        </div>
+      </div>
+    </div>
   );
 }
