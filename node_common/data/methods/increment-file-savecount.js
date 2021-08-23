de79a8b0c844703a0bf35c93f1289b52e4a1964c
Martina <martinalong10@gmail.com>
2021-08-23 16:43:11 -0700

    updated incorrect label for data function

--- node_common/data/methods/increment-file-savecount.js
+++ node_common/data/methods/increment-file-savecount.js
@@ -3,7 +3,7 @@ import { runQuery } from "~/node_common/data/utilities";
 //NOTE(martina): remember to include an ownerId for the file
 export default async ({ id }) => {
   return await runQuery({
-    label: "CREATE_FILE",
+    label: "INCREMENT_FILE_SAVECOUNT",
     queryFn: async (DB) => {
       const query = await DB.from("files").where("id", id).increment("saveCount", 1);
 
@@ -16,7 +16,7 @@ export default async ({ id }) => {
     errorFn: async () => {
       return {
         error: true,
-        decorator: "CREATE_FILE",
+        decorator: "INCREMENT_FILE_SAVECOUNT",
       };
     },
   });
