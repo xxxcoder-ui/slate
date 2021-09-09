647b4d043aad9ea9e678086b83e44d1af589207c
Aminejv <elouartinra@hotmail.com>
2021-09-09 12:21:56 +0100

    feat(upload-utilities): add the ability to cancel uploading and retry after failure

--- common/upload-utilities.js
+++ common/upload-utilities.js
@@ -2,14 +2,30 @@ import * as FileUtilities from "~/common/file-utilities";
 import * as Logging from "~/common/logging";
 import * as Actions from "~/common/actions";
 
-// NOTE(amine): utilities
-export const getFileKey = ({ lastModified, name }) => `${lastModified}-${name}`;
-
 let UploadStore = {
   queue: [],
   failedFilesCache: {},
   isUploading: false,
 };
+
+// NOTE(amine): utilities
+export const getFileKey = ({ lastModified, name }) => `${lastModified}-${name}`;
+
+// NOTE(amine): queue utilities
+const isQueueEmpty = () => UploadStore.queue.length === 0;
+const getFileFromQueue = () => UploadStore.queue.shift() || {};
+const getFileKeysFromQueue = () => UploadStore.queue.map(({ file }) => getFileKey(file));
+const pushFileToQueue = ({ file, slate, bucket }) =>
+  UploadStore.queue.push({ file, slate, bucket });
+const resetQueue = () => (UploadStore.queue = []);
+const removeFileFromQueue = ({ fileKey }) =>
+  (UploadStore.queue = UploadStore.queue.filter(({ file }) => getFileKey(file) !== fileKey));
+
+// NOTE(amine): failedFilesCache utilities
+const storeFileInCache = ({ file, slate, bucketName }) =>
+  (UploadStore.failedFilesCache[getFileKey(file)] = { file, slate, bucketName });
+const deleteFileFromCache = ({ fileKey }) => delete UploadStore.failedFilesCache[fileKey];
+const getFileFromCache = ({ fileKey }) => UploadStore.failedFilesCache[fileKey] || {};
 
 // NOTE(amine): upload factory function
 export function createUploadProvider({
@@ -19,12 +35,13 @@ export function createUploadProvider({
   onProgress,
   onSuccess,
   onError,
+  onCancel,
   onDuplicate,
 }) {
   const scheduleQueueUpload = async () => {
     if (UploadStore.isUploading) return;
 
-    const { file, slate, bucketName } = UploadStore.queue.shift() || {};
+    const { file, slate, bucketName } = getFileFromQueue();
     if (!file) return;
 
     UploadStore.isUploading = true;
@@ -32,25 +49,23 @@ export function createUploadProvider({
     let fileKey = getFileKey(file);
 
     try {
-      // NOTE(amine): delete file from FailedFilesCache
-      delete UploadStore.failedFilesCache[fileKey];
-
-      const response = await FileUtilities.upload({
+      let response = await FileUtilities.upload({
         file,
         bucketName,
         onProgress: (e) => onProgress({ fileKey, loaded: e.loaded }),
       });
 
       if (!response || response.error) {
-        UploadStore.failedFilesCache[fileKey] = file;
+        UploadStore.isUploading = false;
 
+        storeFileInCache({ file, slate, bucketName });
         if (onError) onError({ fileKey });
         return;
       }
 
       let createResponse = await Actions.createFile({ files: [response], slate });
       if (!createResponse || createResponse.error) {
-        UploadStore.failedFilesCache[fileKey] = file;
+        storeFileInCache({ file, slate, bucketName });
       }
 
       const isDuplicate = createResponse?.data?.skipped > 0;
@@ -60,14 +75,15 @@ export function createUploadProvider({
         if (onSuccess) onSuccess({ fileKey });
       }
     } catch (e) {
-      UploadStore.failedFilesCache[fileKey] = file;
-
+      UploadStore.isUploading = true;
+      storeFileInCache({ file, slate, bucketName });
       if (onError) onError({ fileKey });
       Logging.error(e);
     }
 
-    if (UploadStore.queue.length !== 0) {
-      UploadStore.isUploading = false;
+    UploadStore.isUploading = false;
+
+    if (!isQueueEmpty()) {
       scheduleQueueUpload();
       return;
     }
@@ -76,34 +92,38 @@ export function createUploadProvider({
   };
 
   const addToUploadQueue = ({ files, slate, bucketName }) => {
-    if (UploadStore.queue.length === 0 && !UploadStore.isUploading && onStart) onStart();
+    if (isQueueEmpty() && !UploadStore.isUploading && onStart) onStart();
 
-    if (!files || !files.length) {
-      return;
-    }
+    if (!files || !files.length) return;
 
     for (let i = 0; i < files.length; i++) {
-      onAddedToQueue(files[i]);
-      UploadStore.queue.push({ file: files[i], slate, bucketName });
+      if (onAddedToQueue) onAddedToQueue(files[i]);
+      pushFileToQueue({ file: files[i], slate, bucketName });
       scheduleQueueUpload();
     }
   };
 
+  const retry = ({ fileKey }) => {
+    const { file, slate, bucketName } = getFileFromCache({ fileKey });
+    addToUploadQueue({ files: [file], slate, bucketName });
+    deleteFileFromCache({ fileKey });
+  };
+
+  const cancel = ({ fileKey }) => {
+    removeFileFromQueue({ fileKey });
+    if (onCancel) onCancel({ fileKeys: [fileKey] });
+  };
+
+  const cancelAll = () => {
+    const keys = getFileKeysFromQueue();
+    resetQueue();
+    if (onCancel) onCancel({ fileKeys: keys });
+  };
+
   return {
     upload: addToUploadQueue,
+    retry,
+    cancel,
+    cancelAll,
   };
 }
-
-// const cancel = ({ fileKey, context }) => {
-//   if (fileKey === UploadQueueStore.currentFile) {
-//     console.log("still UPLOADING");
-//   }
-//   // TODO UI update
-//   UploadQueueStore.filter((file) => fileKey(file) !== fileKey);
-//   context.setState({});
-// };
-
-// const retry = async ({ fileKey, context }) => {
-//   const file = FailedFilesCache[fileKey];
-//   await upload({ file, context });
-// };
