ef7e8a1103cd25cb8e9e64287ae23c516f6c0005
Aminejv <elouartinra@hotmail.com>
2021-09-15 19:38:06 +0100

    feat(Upload):
    - export Provider
    
    - add Root component (managing the upload modal visibility)
    
    - add Trigger component (responsible for showing the upload modal)

---
+++ components/core/Upload/index.js
@@ -0,0 +1,90 @@
+import * as React from "react";
+import * as Styles from "~/common/styles";
+import * as System from "~/components/system";
+import * as Constants from "~/common/constants";
+import * as SVG from "~/common/svg";
+import * as Events from "~/common/custom-events";
+
+import { useUploadContext } from "~/components/core/Upload/Provider";
+import { Show } from "~/components/utility/Show";
+import { ModalPortal } from "../ModalPortal";
+import { motion } from "framer-motion";
+import { css } from "@emotion/react";
+import { Provider } from "~/components/core/Upload/Provider";
+
+import UploadModal from "~/components/core/Upload/Modal";
+import DataMeter from "~/components/core/DataMeter";
+
+/* -------------------------------------------------------------------------------------------------
+ * Root
+ * -----------------------------------------------------------------------------------------------*/
+const Root = ({ onAction, viewer, children }) => {
+  const [{ isUploadModalVisible }] = useUploadContext();
+  return (
+    <>
+      {children}
+      <Show when={isUploadModalVisible}>
+        <ModalPortal>
+          <UploadModal viewer={viewer} onAction={onAction} />
+        </ModalPortal>
+      </Show>
+    </>
+  );
+};
+
+/* -------------------------------------------------------------------------------------------------
+ * Trigger
+ * -----------------------------------------------------------------------------------------------*/
+
+const Trigger = ({ enableMetrics = false, viewer, css, children, ...props }) => {
+  const showUploadModal = () => {
+    if (!viewer) {
+      Events.dispatchCustomEvent({ name: "slate-global-open-cta", detail: {} });
+      return;
+    }
+    Events.dispatchCustomEvent({ name: "upload-modal-open" });
+  };
+
+  return (
+    <div css={Styles.HORIZONTAL_CONTAINER_CENTERED}>
+      <Show when={enableMetrics}>
+        <UploadMetrics />
+      </Show>
+      <button css={[Styles.BUTTON_RESET, css]} onClick={showUploadModal} {...props}>
+        {children}
+      </button>
+    </div>
+  );
+};
+
+const UploadMetrics = () => {
+  const [{ isUploading, totalBytesUploaded, totalBytes }, { showUploadModal }] = useUploadContext();
+  const uploadProgress = Math.floor((totalBytesUploaded / totalBytes) * 100);
+
+  return (
+    isUploading && (
+      <motion.button
+        initial={{ opacity: 0, y: 10 }}
+        animate={{ opacity: 1, y: 0 }}
+        exit={{ y: 10, opacity: 0 }}
+        css={Styles.BUTTON_RESET}
+        style={{ marginRight: 14 }}
+        aria-label="Upload"
+        onClick={showUploadModal}
+      >
+        <System.P3 color="textBlack">{uploadProgress}%</System.P3>
+        <DataMeter
+          bytes={totalBytesUploaded}
+          maximumBytes={totalBytes}
+          style={{
+            width: 28,
+            marginTop: 4,
+            backgroundColor: Constants.semantic.bgGrayLight,
+          }}
+        />
+      </motion.button>
+    )
+  );
+};
+
+export { Provider, Root, Trigger };
