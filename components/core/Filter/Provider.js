c9eec5b7643992384f326514a75de80d2ee1ad79
Aminejv <elouartinra@hotmail.com>
2021-09-24 18:08:09 +0100

    feat(Filter/Provider):
    - add useFilterSidebar to handle Sidebar visibility
    
    - add useFilter to handle filter state
    
    - add useFilterWorker to handle web worker state

---
+++ components/core/Filter/Provider.js
@@ -0,0 +1,93 @@
+import * as React from "react";
+
+import { useWorker } from "~/common/hooks";
+
+const UploadContext = React.createContext({});
+
+export const useFilterContext = () => React.useContext(UploadContext);
+
+export const Provider = ({ children, viewer }) => {
+  const [isSidebarVisible, toggleSidebar] = useFilterSidebar();
+
+  const [filterState, { setFilterType, setFilterObjects, resetFilterState }] = useFilter({
+    library: viewer.library,
+  });
+
+  const workerState = useFilterWorker({ filterState, setFilterObjects, library: viewer.library });
+
+  const contextValue = React.useMemo(
+    () => [
+      { isSidebarVisible, filterState, ...workerState },
+      { toggleSidebar, setFilterType, resetFilterState },
+    ],
+    [isSidebarVisible, filterState, workerState]
+  );
+
+  return <UploadContext.Provider value={contextValue}>{children}</UploadContext.Provider>;
+};
+
+const useFilterSidebar = () => {
+  const [isSidebarVisible, setSidebarState] = React.useState(false);
+  const toggleSidebar = () => setSidebarState((prev) => !prev);
+  return [isSidebarVisible, toggleSidebar];
+};
+
+const useFilter = ({ library }) => {
+  const DEFAULT_STATE = {
+    view: "initial",
+    subview: undefined,
+    type: "library",
+    objects: library,
+    search: {
+      objects: [],
+      tags: [],
+      startDate: null,
+      endDate: null,
+    },
+  };
+
+  const [filterState, setFilterState] = React.useState(DEFAULT_STATE);
+
+  const setFilterType = ({ view, subview = undefined, type }) =>
+    setFilterState((prev) => ({ ...prev, view, subview, type }));
+
+  const setFilterObjects = (objects) => setFilterState((prev) => ({ ...prev, objects }));
+
+  const resetFilterState = () => setFilterState(DEFAULT_STATE);
+
+  return [filterState, { setFilterType, resetFilterState, setFilterObjects }];
+};
+
+const useFilterWorker = ({ filterState, setFilterObjects, library }) => {
+  const DEFAULT_STATE = { loading: false, error: false };
+  const [workerState, setWorkerState] = React.useState(DEFAULT_STATE);
+
+  const { view, subview, type } = filterState;
+
+  /**
+   * NOTE(amine): Web workers are usually pretty fast,
+   * but if it takes more than 500ms to handle a task, we'll show a loading screen
+   */
+  const timeoutRef = React.useRef();
+
+  useWorker(
+    {
+      path: "../workers/filter-files.js",
+      onStart: (worker) => {
+        worker.postMessage({ objects: library, view, subview, type });
+        timeoutRef.current = setTimeout(() => {
+          setWorkerState((prev) => ({ ...prev, loading: true }));
+        }, 500);
+      },
+      onError: () => setWorkerState((prev) => ({ ...prev, error: true })),
+      onMessage: (e) => {
+        clearTimeout(timeoutRef.current);
+        setWorkerState(DEFAULT_STATE);
+        setFilterObjects(e.data);
+      },
+    },
+    [view, subview, type]
+  );
+
+  return workerState;
+};
