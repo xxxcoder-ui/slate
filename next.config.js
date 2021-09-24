2c601066a6e4eebb65df5f4bd175c4d9f500401f
Aminejv <elouartinra@hotmail.com>
2021-09-24 17:37:44 +0100

    feat(web-workers):
    -  add useWorker to interact with web workers declaratively
    
    - update next to 11.1.2 to support web-workers
    
    - update next config to ignore eslint errors during build

--- next.config.js
+++ next.config.js
@@ -2,6 +2,11 @@ const withOffline = require("next-offline");
 
 const nextConfig = {
   webpack5: true,
+  eslint: {
+    // Warning: This allows production builds to successfully complete even if
+    // your project has ESLint errors.
+    ignoreDuringBuilds: true,
+  },
 };
 
 module.exports = withOffline(nextConfig);
