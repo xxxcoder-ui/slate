8980059a769c3a9e628a0b583d81300b5fe9a37d
Aminejv <elouartinra@hotmail.com>
2021-10-26 19:12:52 +0100

    feat(GlobalCarousel/Jumpers): export all jumpers

---
+++ components/system/components/GlobalCarousel/jumpers/index.js
@@ -0,0 +1,35 @@
+import { FileDescription } from "~/components/system/components/GlobalCarousel/jumpers/FileDescription";
+
+import {
+  MoreInfo,
+  MoreInfoMobile,
+} from "~/components/system/components/GlobalCarousel/jumpers/MoreInfo";
+
+import {
+  EditInfo,
+  EditInfoMobile,
+} from "~/components/system/components/GlobalCarousel/jumpers/EditInfo";
+
+import { Share, ShareMobile } from "~/components/system/components/GlobalCarousel/jumpers/Share";
+
+import {
+  EditChannels,
+  EditChannelsMobile,
+} from "~/components/system/components/GlobalCarousel/jumpers/EditChannels";
+
+export {
+  //NOTE(amine): FileDescription jumper
+  FileDescription,
+  //NOTE(amine): MoreInfo jumper
+  MoreInfo,
+  MoreInfoMobile,
+  //NOTE(amine): EditInfo jumper
+  EditInfo,
+  EditInfoMobile,
+  //NOTE(amine): Share jumper
+  Share,
+  ShareMobile,
+  //NOTE(amine): EditChannels jumper
+  EditChannels,
+  EditChannelsMobile,
+};
