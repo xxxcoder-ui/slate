344336110857fc7bf8f5f6d90f15c7254b8d2969
Aminejv <elouartinra@hotmail.com>
2022-01-02 13:53:03 +0100

    feat(hooks): add accessibility hooks

--- common/hooks/index.js
+++ common/hooks/index.js
@@ -517,3 +517,21 @@ export const useLocalStorage = (key) => ({
   getItem: () => localStorage?.getItem(key),
   removeItem: () => localStorage?.removeItem(key),
 });
+
+export const useCombinedRefs = (...refs) => {
+  const targetRef = React.useRef();
+
+  React.useEffect(() => {
+    refs.forEach((ref) => {
+      if (!ref) return;
+
+      if (typeof ref === "function") {
+        ref(targetRef.current);
+      } else {
+        ref.current = targetRef.current;
+      }
+    });
+  }, [refs]);
+
+  return targetRef;
+};
